33d2e740faf2a219b0b31fe39babdf9403a9306f
rjdkirk <82522520+rjdkirk@users.noreply.github.com>
2022-07-14 16:26:56 +0100

    Update Spotify_sleep_timbre.py

--- Spotify_sleep_timbre.py
+++ Spotify_sleep_timbre.py
@@ -3,8 +3,8 @@ import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = 'bc14dbad3a614e6bac21a603e03e343c'
-client_secret = 'd6e20186fd5448f384012f42756b7f5a'
+client_id = 'xxxx'
+client_secret = 'xxxx'
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
